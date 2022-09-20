d2f3faf4f53896d47788236af13c4e1bb6dfd7db
laoona <laoona@163.com>
2022-09-20 17:05:58 +0800

    config: 更新

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "template": "smarty",
+    "smarty": {
+        "dataManifest": {},
+        "constPath": "./const.json"
+    },
+    "spritesmith": {
+        "REM": true
+    },
+    "tinypng": false,
+    "babel": false,
+    "devServer": {
+        "proxy": {}
+    },
+}
